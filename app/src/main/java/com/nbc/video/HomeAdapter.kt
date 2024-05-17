b1be05fd735ea9bd38e3d80ab7ffd2d58e40e3dc
hyejeong <0601a@naver.com>
2024-05-17 12:25:14 +0900

    feat: HomeAdapter.kt, HomeFragment.kt 수정해서 작동 잘됨(더미데이터 넣은거)

--- app/src/main/java/com/nbc/video/HomeAdapter.kt
+++ app/src/main/java/com/nbc/video/HomeAdapter.kt
@@ -1,26 +1,17 @@
 package com.nbc.video
 
-import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.nbc.video.data.Home
-import com.nbc.video.data.Item
+import com.nbc.video.data.CategoryVideo
+import com.nbc.video.data.ChannelVideo
+import com.nbc.video.data.PopularVideo
 import com.nbc.video.databinding.VideoItemBinding
-import com.nbc.video.network.NetworkDataSource
 
-class HomeAdapter(private val mContext: Context) :
-    RecyclerView.Adapter<HomeAdapter.VideoViewHolder>() {
-    //비디오 목록
-    var videos = ArrayList<Home>()
-    var items = ArrayList<Item>()
-
-    private lateinit var binding: VideoItemBinding
+class HomeAdapter<T>(
+    private val items: List<T>,
+) : RecyclerView.Adapter<HomeAdapter.VideoViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoViewHolder {
         val binding = VideoItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -28,42 +19,40 @@ class HomeAdapter(private val mContext: Context) :
     }
 
     override fun onBindViewHolder(holder: VideoViewHolder, position: Int) {
-        /*var currentVideo = videos[position]
-        var item = items[position]
-
-        holder.iv_video.setImageURI(item.thumbnailUrl.toUri()) //이거 맞나?
-        holder.tv_title.text = item.title
-*/
-        /*Glide.with(mContext)
-            .load(item.thumbnailUrl)
-            .load(item.title)
-*/
-
+        holder.bind(items[position])
     }
 
-    /*override fun onBindViewHolder(holder: BaseRcvViewHolder, position: Int) {
-        (holder.binding() as ItemYoutubeSearchBinding).apply {
-            this.vm = getItem(position)
 
-            //Using Glide Library(for image load).
-            vm?.snippet?.thumbnails?.medium?.url?.let {
-                Glide.with(holder.itemView).load(it).into(this.ivTitle)
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    class VideoViewHolder(private val binding: VideoItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun <T> bind(item: T) {
+            when (item) {
+                is CategoryVideo -> {
+                    binding.tvTitle.text = item.title
+                    Glide.with(itemView.context)
+                        .load(item.thumbnails.medium.url)
+                        .into(binding.ivVideo)
+                }
+
+                is PopularVideo -> {
+                    binding.tvTitle.text = item.title
+                    Glide.with(itemView.context)
+                        .load(item.thumbnails.medium.url)
+                        .into(binding.ivVideo)
+                }
+
+                is ChannelVideo -> {
+                    binding.tvTitle.text = item.title
+                    Glide.with(itemView.context)
+                        .load(item.thumbnails.medium.url)
+                        .into(binding.ivVideo)
+                }
             }
         }
-    }*/
-
-
-    override fun getItemCount(): Int = videos.size
-
-    inner class VideoViewHolder(binding: VideoItemBinding) : RecyclerView.ViewHolder(binding.root),
-        View.OnClickListener {
-        var iv_video: ImageView = binding.ivVideo
-        var tv_title: TextView = binding.tvTitle
-        override fun onClick(v: View?) {
-            val position = adapterPosition.takeIf { it != RecyclerView.NO_POSITION } ?: return
-        }
-
-
     }
 
 }
--- app/src/main/java/com/nbc/video/HomeFragment.kt
+++ app/src/main/java/com/nbc/video/HomeFragment.kt
@@ -2,39 +2,26 @@ package com.nbc.video
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.nbc.video.data.Home
+import com.nbc.video.data.DummyData
 import com.nbc.video.databinding.FragmentHomeBinding
 import com.nbc.video.network.NetworkDataSource
-import com.nbc.video.network.model.YouTubeResponse
 import com.nbc.video.network.model.video.enums.NetworkVideoChart
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     private lateinit var mContext: Context
-    private var homeAdapter: HomeAdapter? = null
-    private var homeItems: ArrayList<Home> = ArrayList()
 
     private lateinit var networkDataSource: NetworkDataSource
-
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         mContext = context
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -48,48 +35,34 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setRecyclerView()
+        //popular
+        binding.rlPopular.adapter = HomeAdapter(DummyData.getDummyPopularVideos())
+        binding.rlPopular.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        //category
+        binding.rlCategory.adapter = HomeAdapter(DummyData.getDummyCategoryVideos())
+        binding.rlCategory.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
 
+        //channel
+        binding.rlChannel.adapter = HomeAdapter(DummyData.getDummyChannelVideos())
+        binding.rlChannel.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+
+
+        //setRecyclerView()
     }
 
     //Most Popular 영상 리스트
-    suspend fun popularVideos() {
+    /*suspend fun popularVideos() {
         val response = networkDataSource.getVideos(
             chart = NetworkVideoChart.MOST_POPULAR, // MOST_POPULAR,
         )
-view.apply {
 
-}
-
-    }
-    /*private fun fetchImageResults(query: String) {
-        apiService.image_search(Constants.AUTH_HEADER, query, "recency", 1, 80)
-            ?.enqueue(object : Callback<ImageModel?> {
-                override fun onResponse(call: Call<ImageModel?>, response: Response<ImageModel?>) {
-                    response.body()?.meta?.let { meta ->
-                        if (meta.totalCount > 0) {
-                            response.body()!!.documents.forEach { document ->
-                                val title = document.displaySitename
-                                val datetime = document.datetime
-                                val url = document.thumbnailUrl
-                                resItems.add(SearchItemModel(title, datetime, url))
-                            }
-                        }
-                    }
-                    adapter.items = resItems
-                    adapter.notifyDataSetChanged()
-                }
-
-                override fun onFailure(call: Call<ImageModel?>, t: Throwable) {
-                    Log.e("#jblee", "onFailure: ${t.message}")
-                }
-            })
     }*/
 
-
     //리사이클러뷰 레이아웃 설정
-    fun setRecyclerView() {
-
+    /*fun setRecyclerView() {
         binding.rlPopular.adapter = homeAdapter
         binding.rlPopular.layoutManager =
             LinearLayoutManager(mContext).also {
@@ -109,5 +82,5 @@ view.apply {
                 it.orientation = LinearLayoutManager.HORIZONTAL
             }
 
-    }
+    }*/
 }
