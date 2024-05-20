4caa66de768f26f5d7cc7e4a18f1540420aab2cb
agvber <agvber12@gmail.com>
2024-05-20 10:52:46 +0900

    rollback: MainActivity 롤백

--- app/src/main/java/com/nbc/video/MainActivity.kt
+++ app/src/main/java/com/nbc/video/MainActivity.kt
@@ -3,17 +3,18 @@ package com.nbc.video
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.commit
-import com.nbc.video.main.SearchFragment
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        supportFragmentManager.commit {
-            add(R.id.fl_items, SearchFragment())
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 }
