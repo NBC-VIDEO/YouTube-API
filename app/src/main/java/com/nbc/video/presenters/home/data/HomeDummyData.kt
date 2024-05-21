fbce7176ca138f41eb60a68b0484ab1b55bc0853
hyejeong <0601a@naver.com>
2024-05-21 18:12:30 +0900

    feat: HomeDummyData 와 VideoHomeModel 코드 정리

--- app/src/main/java/com/nbc/video/presenters/home/data/HomeDummyData.kt
+++ app/src/main/java/com/nbc/video/presenters/home/data/HomeDummyData.kt
@@ -8,8 +8,6 @@ import com.nbc.video.presenters.home.model.Thumbnail
 import com.nbc.video.presenters.home.model.VideoHomeModel
 
 // 싱글톤 객체 정의
-
-
 object HomeDummyData {
     private lateinit var _video: VideoHomeModel
     val video: VideoHomeModel get() = _video
@@ -27,7 +25,7 @@ object HomeDummyData {
                         320,
                         180
                     ),
-                    height = Image(
+                    high = Image(
                         "https://cdn.pixabay.com/photo/2018/10/01/09/21/pets-3715733_1280.jpg",
                         480,
                         360
@@ -47,7 +45,7 @@ object HomeDummyData {
                         320,
                         180
                     ),
-                    height = Image(
+                    high = Image(
                         "https://cdn.pixabay.com/photo/2016/09/05/21/37/cat-1647775_1280.jpg",
                         480,
                         360
@@ -67,7 +65,7 @@ object HomeDummyData {
                         320,
                         180
                     ),
-                    height = Image(
+                    high = Image(
                         "https://cdn.pixabay.com/photo/2016/09/05/21/37/cat-1647775_1280.jpg",
                         480,
                         360
@@ -87,7 +85,7 @@ object HomeDummyData {
                         320,
                         180
                     ),
-                    height = Image(
+                    high = Image(
                         "https://cdn.pixabay.com/photo/2016/09/05/21/37/cat-1647775_1280.jpg",
                         480,
                         360
@@ -112,7 +110,7 @@ object HomeDummyData {
                         320,
                         180
                     ),
-                    height = Image(
+                    high = Image(
                         "https://cdn.pixabay.com/photo/2016/09/05/21/37/cat-1647775_1280.jpg",
                         480,
                         360
@@ -132,7 +130,7 @@ object HomeDummyData {
                         320,
                         180
                     ),
-                    height = Image(
+                    high = Image(
                         "https://cdn.pixabay.com/photo/2016/09/05/21/37/cat-1647775_1280.jpg",
                         480,
                         360
@@ -152,7 +150,7 @@ object HomeDummyData {
                         320,
                         180
                     ),
-                    height = Image(
+                    high = Image(
                         "https://cdn.pixabay.com/photo/2016/09/05/21/37/cat-1647775_1280.jpg",
                         480,
                         360
@@ -172,7 +170,7 @@ object HomeDummyData {
                         320,
                         180
                     ),
-                    height = Image(
+                    high = Image(
                         "https://cdn.pixabay.com/photo/2016/09/05/21/37/cat-1647775_1280.jpg",
                         480,
                         360
@@ -197,7 +195,7 @@ object HomeDummyData {
                         320,
                         180
                     ),
-                    height = Image(
+                    high = Image(
                         "https://cdn.pixabay.com/photo/2016/09/05/21/37/cat-1647775_1280.jpg",
                         480,
                         360
@@ -217,7 +215,7 @@ object HomeDummyData {
                         320,
                         180
                     ),
-                    height = Image(
+                    high = Image(
                         "https://cdn.pixabay.com/photo/2016/09/05/21/37/cat-1647775_1280.jpg",
                         480,
                         360
@@ -237,7 +235,7 @@ object HomeDummyData {
                         320,
                         180
                     ),
-                    height = Image(
+                    high = Image(
                         "https://cdn.pixabay.com/photo/2016/09/05/21/37/cat-1647775_1280.jpg",
                         480,
                         360
@@ -257,7 +255,7 @@ object HomeDummyData {
                         320,
                         180
                     ),
-                    height = Image(
+                    high = Image(
                         "https://cdn.pixabay.com/photo/2016/09/05/21/37/cat-1647775_1280.jpg",
                         480,
                         360
--- app/src/main/java/com/nbc/video/presenters/home/model/VideoHomeModel.kt
+++ app/src/main/java/com/nbc/video/presenters/home/model/VideoHomeModel.kt
@@ -7,43 +7,37 @@ data class VideoHomeModel(
     val categoryList: List<Category> //카테고리 목록 리스트
 )
 
-//예시 - ("동물","카테고리아이디a"),("식물","카테고리아이디b")
 data class Category(
-    val categoryName: String, //카테고리 이름있어야 하지 않나?
+    val categoryTitle: String, //카테고리 이름
     val categoryId: String,
-    val channelId: String   //snippet의 channelId
-    //Q. 카테고리 고른 후 영상에 카테고리 아이디가 포함되어야하니까 ?
-    //Q. 카테고리 리스트에 채널 아이디 필요없지 않나?
-    //Q. categoryId가 근데 카테고리 이름이랑 같은 건가??
+    val channelId: String
 )
 
-//인기 있는 비디오 리스트
+//1. 인기 있는 비디오 리스트
 data class PopularVideo(
     val thumbnails: Thumbnail,
     val title: String
 )
 
 
-//카테고리 선택후 비디오 리스트
+//2. 카테고리 선택후 비디오 리스트
 data class CategoryVideo(
     val thumbnails: Thumbnail,
     val title: String,
     val categoryId: String //카테고리 아이디
 )
 
-//카테고리 선택 후 나온 채널 리스트
+//3. 카테고리 선택 후 채널 리스트
 data class ChannelVideo(
-    val thumbnails: Thumbnail, //썸네일이 동영상 썸네일이 아닌 채널 프로필 사진....
+    val thumbnails: Thumbnail,
     val title: String,  // 채널의 이름
     val channelId: String
-    //카테고리 아이디 가져야하지 않나? 카테고리 아이디를 검색했을 때 해당하는 채널이 나오는거 아닌가?
-    //채널 아이디
 )
 
 data class Thumbnail(
     val default: Image,
     val medium: Image,
-    val height: Image,
+    val high: Image,
 )
 
 data class Image(
